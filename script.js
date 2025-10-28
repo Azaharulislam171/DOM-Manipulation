ab382d98e63c4fded7122a7c2e9007a36edff51b
Azaharul Islam <Azaharulislam171@gmail.com>
2025-10-28 09:44:25 -0400

    added an image

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-Document.getElementById("taskname1").innerHTML="Complete the project documentation";
+document.getElementById("taskname1").innerHTML="Complete the project documentation";
+// what is the difference between Document and document in DOM
+
+// In the Document Object Model (DOM), "Document" refers to the interface that represents the entire HTML or XML document. It is a global object that provides methods and properties to interact with the content of the document, such as creating elements, accessing nodes, and manipulating the structure of the document.
+
+document.getElementById("compl1").innerHTML="Complete the project documentation";
+document.getElementById("taskname2").innerHTML="Review code for bugs";
+
+let task=document.getElementById("taskname2").innerHTML;
+console.log(task+" "+"is selected");
