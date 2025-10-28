003bb9b1efe3643c09e413a2cea975d73693a0d6
Azaharul Islam <Azaharulislam171@gmail.com>
2025-10-28 14:29:14 -0400

    s

--- script.js
+++ script.js
@@ -3,8 +3,22 @@ document.getElementById("taskname1").innerHTML="Complete the project documentati
 
 // In the Document Object Model (DOM), "Document" refers to the interface that represents the entire HTML or XML document. It is a global object that provides methods and properties to interact with the content of the document, such as creating elements, accessing nodes, and manipulating the structure of the document.
 
-document.getElementById("compl1").innerHTML="Complete the project documentation";
+document.getElementById("compl1").innerHTML="Remove";
 document.getElementById("taskname2").innerHTML="Review code for bugs";
 
+// i declared a variable and added something to print to the console
 let task=document.getElementById("taskname2").innerHTML;
 console.log(task+" "+"is selected");
+
+document.getElementById("compl1").addEventListener("click",function(){
+    document.getElementById("taskname2").style.textDecoration="line-through";
+    document.getElementById("taskname1").style.textDecoration="line-through";
+});
+
+
+document.getElementById("addTask").addEventListener("click", function () {
+    event.preventDefault(); 
+    let newTask = document.getElementById("new-task").value; // Fixed typo
+    console.log(newTask);
+    document.getElementById("taskname3").innerHTML = newTask;
+});
